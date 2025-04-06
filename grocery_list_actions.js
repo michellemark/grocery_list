5702fdd736c9ea9f28470641ec9a6d1c3e18e9db
Michelle Mark <web-developer@michellemark.me>
2025-04-06 13:23:32 -0400

    update to the latest versions of jquery, bootstrap and popper.  add a few new features: ability to remove an item from the list with a trash can icon. print button that only prints the list, hides the trash can and adds check boxes to the left of each item.

--- grocery_list_actions.js
+++ grocery_list_actions.js
@@ -1,6 +1,4 @@
-
-
-var list_processor = ( function() {
+var list_processor = (function () {
     var grocery_list = [];
     var departments = [
         "Bakery", "Baking", "Beauty", "Breakfast", "Canned Goods", "Condiments",
@@ -15,8 +13,29 @@ var list_processor = ( function() {
     var reset_list_btn = $("#reset_list_btn");
     var new_department = $("#new_department");
     var add_department_btn = $("#add_department_btn");
+    var print_list_btn = $("#print_list_btn"); // Reference the existing print button
 
-    function clean_user_input (value) {
+    // Add print CSS rules
+    function add_print_styles() {
+        // Check if our style is already added
+        if ($('#print-style-custom').length === 0) {
+            $('head').append(
+                '<style id="print-style-custom" type="text/css">' +
+                '@media print {' +
+                '  .delete-item { display: none !important; }' +
+                '  .checkbox-print { display: inline-block !important; margin-right: 10px; }' +
+                '  .checkbox-container { display: flex; align-items: center; }' +
+                '  .checkbox-print { height: 16px; width: 16px; border: 1px solid #000; }' +
+                '}' +
+                '@media screen {' +
+                '  .checkbox-print { display: none; }' +
+                '}' +
+                '</style>'
+            );
+        }
+    }
+
+    function clean_user_input(value) {
         value = value.replace(/[^a-zA-Z0-9\s\/\-_]/, "");
 
         return value.replace(/\w\S*/g, function (word) {
@@ -24,7 +43,7 @@ var list_processor = ( function() {
         });
     }
 
-    function load_department_selections () {
+    function load_department_selections() {
         department_selection.empty();
         departments.sort();
 
@@ -35,39 +54,77 @@ var list_processor = ( function() {
 
             if (i === 0) {
                 option = new Option(item, item, true, true);
-            }
-            else {
+            } else {
                 option = new Option(item, item);
             }
             department_selection.append(option);
         }
     }
 
-    function print_grocery_list() {
+    // Remove an item from the grocery list
+    function remove_item(index) {
+        grocery_list.splice(index, 1);
+        display_grocery_list(); // Updated function name
+    }
+
+    // Renamed from print_grocery_list to display_grocery_list
+    function display_grocery_list() {
         display_list.empty();
+
+        if (grocery_list.length === 0) {
+            display_list.append("Nothing in the list, add some items!");
+            return;
+        }
+
         grocery_list.sort(function (a, b) {
             return a.department.localeCompare(b.department) || a.item.localeCompare(b.item);
         });
+
         var current_department = null;
 
         for (var i = 0; i < grocery_list.length; i++) {
-            next_item = grocery_list[i];
+            var next_item = grocery_list[i];
 
             if (next_item.department !== current_department) {
-
                 if (next_item.department) {
                     current_department = next_item.department;
-                }
-                else {
+                } else {
                     current_department = "No Department Specified";
                 }
 
                 display_list.append("<h4 class='mt-4'>" + current_department + "</h4>");
             }
 
-            display_list.append("<span class='pl-3'>" + next_item.item + "</span><br>");
+            // Add item with checkbox for print and trash icon for screen
+            display_list.append(
+                "<div class='d-flex align-items-center'>" +
+                "<div class='checkbox-container'>" +
+                "<div class='checkbox-print'></div>" +
+                "<span class='ps-3 flex-grow-1'>" + next_item.item + "</span>" +
+                "</div>" +
+                "<button class='btn btn-sm text-danger delete-item' data-index='" + i + "'>" +
+                "<i class='fa fa-trash'></i>🗑️" +
+                "</button>" +
+                "</div>"
+            );
         }
 
+        // Set up delete button handlers
+        $(".delete-item").on("click", function () {
+            var index = $(this).data("index");
+            remove_item(index);
+        });
+    }
+
+    // New function to handle printing only the grocery list
+    function print_only_list() {
+        if (grocery_list.length === 0) {
+            alert("Nothing to print. Add some items to your grocery list first.");
+            return;
+        }
+
+        // Use the browser's print function
+        window.print();
     }
 
     function validate_input(text_input) {
@@ -77,8 +134,7 @@ var list_processor = ( function() {
             is_valid = true;
             text_input.removeClass("is-invalid");
             text_input.addClass("is-valid");
-        }
-        else {
+        } else {
             text_input.removeClass("is-valid");
             text_input.addClass("is-invalid");
         }
@@ -88,9 +144,13 @@ var list_processor = ( function() {
 
     return {
         setup_page: function () {
+            // Add print styles at initialization
+            add_print_styles();
+
             item_entry.on("keyup", function () {
                 validate_input($(this));
             });
+
             save_new_btn.on("click", function () {
                 var is_valid = validate_input(item_entry);
 
@@ -103,35 +163,63 @@ var list_processor = ( function() {
                         "item": new_item,
                         "department": dept
                     });
-                    print_grocery_list();
-                }
+                    display_grocery_list(); // Updated function name
 
+                    // Clear the input field after adding item
+                    item_entry.val('');
+                    item_entry.removeClass('is-valid');
+                }
             });
+
             reset_list_btn.on("click", function () {
+                grocery_list = []; // Empty the grocery list array
                 display_list.empty();
                 display_list.append("Nothing in the list, add some items!");
+                item_entry.val('');
+                item_entry.removeClass('is-valid is-invalid');
+                new_department.val('');
+                new_department.removeClass('is-valid is-invalid');
             });
-            new_department.on("keyup", function() {
+
+            new_department.on("keyup", function () {
                 validate_input($(this));
             });
+
             add_department_btn.on("click", function () {
                 var is_valid = validate_input(new_department);
 
                 if (is_valid) {
                     var new_item = new_department.val();
                     new_item = clean_user_input(new_item);
-                    departments.push(new_item);
-                    load_department_selections();
-                }
 
+                    // Check if department already exists
+                    if (departments.indexOf(new_item) === -1) {
+                        departments.push(new_item);
+                        load_department_selections();
+
+                        // Clear the input field after adding department
+                        new_department.val('');
+                        new_department.removeClass('is-valid');
+                    } else {
+                        // Alert user if department already exists
+                        alert("This department already exists!");
+                    }
+                }
             });
+
+            // Set up the print button click handler
+            print_list_btn.on("click", function () {
+                print_only_list();
+            });
+
+            // Initialize page
             reset_list_btn.trigger("click");
             load_department_selections();
         }
     };
 }(list_processor || {}));
 
-
-$.when($.ready).then( function() {
+// jQuery ready syntax
+$(function () {
     list_processor.setup_page();
 });
